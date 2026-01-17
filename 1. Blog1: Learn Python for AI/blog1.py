ab88a4795073c393f43a5197885f8e19b148216e
atognoo_b <hoanganhvu100402@gmail.com>
2026-01-17 14:17:34 +0700

    update

--- 1. Blog1: Learn Python for AI/blog1.py
+++ 1. Blog1: Learn Python for AI/blog1.py
@@ -1,25 +1,5 @@
-'''
-<div align="center">
+a = 1
+b = 2
+c = 3
 
-</div>
-
-'''
-
-# Creating a tuple
-person = ("name", 24, "AIO")
-
-# Accessing elements (same as lists)
-print(person[0])  # Output: name
-
-# Tuples are immutable
-
-# Unpacking a tuple
-name, age, occupation = person
-print(age)  # Output: 24
-# Creating a single-item tuple (note the comma)
-single_tuple = (42,)
-
-# Tuple methods
-coordinates = (10, 20, 10, 30)
-print(coordinates.count(10))  # Output: 2
-print(coordinates.index(20))  # Output: 1
+print(a + b + c)
